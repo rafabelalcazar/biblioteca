090f200e2f44710a4531b427cea437ddc0fb6232
rafabelalcazar <38873104+rafabelalcazar@users.noreply.github.com>
2019-06-05 20:48:08 -0500

    Se agregaron las imagenes perdidas en el merge

--- js/libros.js
+++ js/libros.js
@@ -2,20 +2,33 @@ alert('hola')
 search = document.getElementById('search-book')
 books = document.getElementsByClassName('card-title')
 
-var i;
-for (i = 0; i < books.length; i++) {
-    console.log(books[i].innerText)
-}
-function booksTitle() {
+// Recibe lo que se escriba en searchbar y el objeto de libros
+function compararTitulo(search, books) {
+    for (let i = 0; i < books.length; i++) {
+        console.log(books[i].innerText.includes(search.value))
+        if (books[i].innerText.includes(search.value)) {
+            // Va hasta el card del libro y le quita la clase display none d-none
+            books[i].parentNode.parentNode.parentNode.parentNode.classList.remove('d-none')
+            // return books[i].parentNode.parentNode.parentNode.parentNode.classList.remove('d-none')
+        }
+        else {
+            books[i].parentNode.parentNode.parentNode.parentNode.classList.add('d-none')
+        }
+
+        // console.log(books[i].innerText)
+    }
 }
 
-console.log(booksTitle)
+
+function comparar() {
+    compararTitulo(search, books)
+}
+
+
+
+
+// console.log(libros(1))
 
 // console.log(books.map(function (book) {
 //     return book.innerText
 // }))
-
-function searchBooks() {
-    // if (search !=)
-
-}
