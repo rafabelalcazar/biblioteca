86ced333b1d8f199cfc7a1906a461eef47fbf72d
rafabelalcazar <38873104+rafabelalcazar@users.noreply.github.com>
2019-06-05 19:49:20 -0500

    Revert "Busqueda por Titulo lista"
    This reverts commit 3c92e3bdd407dd653b64edb7dc1c3f8fc52392ae.

--- js/libros.js
+++ js/libros.js
@@ -2,33 +2,20 @@ alert('hola')
 search = document.getElementById('search-book')
 books = document.getElementsByClassName('card-title')
 
-// Recibe lo que se escriba en searchbar y el objeto de libros
-function compararTitulo(search, books) {
-    for (let i = 0; i < books.length; i++) {
-        console.log(books[i].innerText.includes(search.value))
-        if (books[i].innerText.includes(search.value)) {
-            // Va hasta el card del libro y le quita la clase display none d-none
-            books[i].parentNode.parentNode.parentNode.parentNode.classList.remove('d-none')
-            // return books[i].parentNode.parentNode.parentNode.parentNode.classList.remove('d-none')
-        }
-        else {
-            books[i].parentNode.parentNode.parentNode.parentNode.classList.add('d-none')
-        }
-
-        // console.log(books[i].innerText)
-    }
+var i;
+for (i = 0; i < books.length; i++) {
+    console.log(books[i].innerText)
+}
+function booksTitle() {
 }
 
-
-function comparar() {
-    compararTitulo(search, books)
-}
-
-
-
-
-// console.log(libros(1))
+console.log(booksTitle)
 
 // console.log(books.map(function (book) {
 //     return book.innerText
 // }))
+
+function searchBooks() {
+    // if (search !=)
+
+}
