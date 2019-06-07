429c01090213e399eb80b1bad51c95519791db79
rafabelalcazar <38873104+rafabelalcazar@users.noreply.github.com>
2019-06-06 23:42:28 -0500

    Conexion con db.json y despliegue en HTML
    Conexion con db.json y despliegue en HTML.
    Falta estructurar las consultas para cada caso(busqueda por titulo,categoria, autor,ISBN)

--- js/libros.js
+++ js/libros.js
@@ -26,6 +26,48 @@ function comparar() {
 
 
 
+function searchTitle() {
+    console.log('estoy dentro de la funcion')
+    const xhttp = new XMLHttpRequest()
+    xhttp.open('GET', 'db.json', true)
+    xhttp.send()
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let books = JSON.parse(this.responseText)
+            let containerCards = document.getElementById('card-container')
+            // console.log(containerCards)
+
+            for (let item of books) {
+                console.log(containerCards)
+                containerCards.innerHTML += `
+                <div class="card mb-3 w-100">
+                    <div class="row no-gutters">
+                        <div class="col-md-2 my-auto mx-auto">
+                            <img src="${item.img}" class="card-img cover" alt="">
+                        </div>
+                        <div class="col-md-7 my-auto">
+                            <div class="card-body">
+                                <h5 class="card-title">${item.titulo}</h5>
+                                <p class="card-text">${item.autor}</p>
+                                <p>Año: <span>${item.año}</span></p>
+                                <p>ISBN: <span>${item.isbn}</span></p>
+                            </div>
+                        </div>
+                        <div class="col-2 my-auto mx-auto w-100 text-center">
+                            <button type="button" class="btn btn-warning">
+                                Disponibles <span class="badge badge-light">${item.disponibles}</span>
+                            </button>
+                            <button class="btn btn-primary my-4">Reservar</button>
+                        </div>
+                    </div>
+                </div>                
+                `
+            }
+        }
+    }
+}
+
+
 
 // console.log(libros(1))
 
